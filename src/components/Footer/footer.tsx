3dbf8694a15041db978aacf07b776cfef09dbe5e
De Roover Justin <justinderoover@gmail.com>
2024-01-29 22:04:35 +0100

    f

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function footer() {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <p>© {new Date().getFullYear()} Sushi Masta. Tous droits réservés.</p>
+    </div>
+  );
 }
