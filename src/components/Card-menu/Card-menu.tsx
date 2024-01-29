c19a99fec6472a36c40a8ae85e41a14b43436c34
De Roover Justin <justinderoover@gmail.com>
2024-01-29 21:33:58 +0100

    add

--- src/components/Card-menu/Card-menu.tsx
+++ src/components/Card-menu/Card-menu.tsx
@@ -1,6 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
-export default function Card_menu({ title, image }: { title: string, image: string }) {
+export default function Card_menu({
+  title,
+  image,
+}: {
+  title: string;
+  image: string;
+}) {
   return (
     <div className="card">
       <img src={image} alt={title} />
