56a2a20781e475f990193b954590923225e31895
De Roover Justin <justinderoover@gmail.com>
2024-01-29 21:56:40 +0100

    f

---
+++ src/app/about/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <p>
+        Sushi Masta is a leading sushi restaurant offering an unparalleled
+        culinary experience. Our team of experienced chefs are passionate about
+        creating authentic sushi with a modern twist. With years of experience
+        in the culinary field, our chefs bring a wealth of knowledge and skills
+        to every dish they create.
+      </p>
+      <p>
+        We use only the freshest and highest quality ingredients for our dishes.
+        Our varied menu offers a selection of traditional and innovative dishes,
+        there's something for everyone. From the classic California roll to our
+        unique house specials, our menu is designed to cater to all tastes and
+        preferences. We also offer a range of vegetarian and gluten-free options
+        to ensure that everyone can enjoy our delicious sushi.
+      </p>
+      <p>
+        Come visit us to find out why we are the preferred choice of sushi
+        lovers in the region. Our friendly and knowledgeable staff are always on
+        hand to provide excellent service and answer any questions you may have.
+        Whether you're a sushi connoisseur or trying sushi for the first time,
+        we're confident that you'll love the Sushi Masta experience.
+      </p>
+      <p>
+        In addition to our dine-in service, we also offer take-out and delivery
+        options so you can enjoy our sushi from the comfort of your own home.
+        We're committed to providing a memorable dining experience, whether
+        you're dining in with us or enjoying our sushi at home.
+      </p>
+    </div>
+  );
+}
